24edeb81936422e2e0d247e43a8655177100e64d
emetdas <72676990+emetdas@users.noreply.github.com>
2020-11-01 06:30:48 +0600

    math object in javascript

---
+++ 03-november/math-object.js
@@ -0,0 +1,23 @@
+const num1 = 10;
+const num2 = 19;
+let val;
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.40);
+val = Math.sqrt(64);
+val = Math.abs(94);
+val = Math.pow(9,2);
+val = Math.min(10,2,10,1,-9);
+val = Math.max(10,2,10,1,-9);
+val = Math.random() * 20;
+val = Math.floor(Math.random() * 20);
+console.log(val);
