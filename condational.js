6218840147924255f768a8d8d035b1668642a1a2
emetdas <emet@b2bleadstaff.com>
2020-12-16 20:31:51 +0600

    change

---
+++ condational.js
@@ -0,0 +1,9 @@
+// var x = confirm("You assess this website");
+// console.log(x);
+var age = 29;
+if (age>10) {
+    console.log("you game play");
+}
+else{
+    console.log("you not play game");
+}
