afddc148d1a52d32639856ae0b5364c8df8efca4
emetdas <emet@b2bleadstaff.com>
2020-12-16 21:29:31 +0600

    javascript learngin stop

--- condational.js
+++ condational.js
@@ -1,9 +1,46 @@
 // var x = confirm("You assess this website");
 // console.log(x);
-var age = 29;
-if (age>10) {
-    console.log("you game play");
-}
-else{
-    console.log("you not play game");
-}
+// var age = 29;
+// if (age>10) {
+//     console.log("you game play");
+// }
+// else{
+//     console.log("you not play game");
+// }
+// if (age<10) {
+//     console.log(`age is les than ${age}`);
+// }
+// else if(age>20){
+//     console.log("age greater than 10");
+// }
+
+// logical oparator
+// && || !
+// var name = "Emet";
+// var age = 19;
+// if (age>18 && name[0]=== 'E'){
+// console.log("you can play this game");    
+// }
+// else{
+//     console.log("you not play this game");
+// }
+// thranary oparator ? :
+// var age = 20;
+// age>19 ? console.log("your age is 20") : console.log("age is default");
+// squance of trinary oparator
+// var age = 18;
+// (age<10) ? console.log("age is less-than 10"):
+// (age<19) ? console.log("age is perfect"):
+// (age<16) ? console.log("age is too short"):
+// console.log("default")
+// var names = 1;
+// switch (names){
+//     case 1:
+//         console.log("Emet is good Developer");
+//         break;
+//     case 2:
+//         console.log("Developer");    
+//         break;
+//     default:
+//     console.log("none");
+// }
